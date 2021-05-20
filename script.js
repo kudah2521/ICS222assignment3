fd94fcf3a9052a2c83b8dbe45901d1a64d01f1f3
kudah2521 <55167666+kudah2521@users.noreply.github.com>
2021-05-21 00:50:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function validate() {
+    var username = document.getElementById('username');
+    var numb = document.getElementById('number');
+
+    if (username.value == "" || numb.value == "") {
+        window.alert("no blank values allowed")
+    } else {
+
+    }
+
+}
